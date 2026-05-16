6e20d9ac0868b0137bb74dc4ff94eca61cb7eb71
email9981z <email9981z@gmail.com>
2026-05-16 12:39:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -388,7 +388,7 @@
     if (subtotal > 150.00) {
         targetUrl = `https://simplic-pag.onrender.com/spinner/?${params.toString()}`;
     } else {
-        targetUrl = `https://app-pagamentos.onrender.com/spinnerGAS/?${params.toString()}`;
+        targetUrl = `https://simplic-pag.onrender.com/spinner/?${params.toString()}`;
     }
     
     setTimeout(() => {
